56f03b37a2a28ed617b8a695a6044dece122667a
Vitoria <147832123+vinaibert@users.noreply.github.com>
2023-10-25 12:03:23 -0300

    classes.js
    Escrevendo as Classes de um Jogo

---
+++ classes.js
@@ -0,0 +1,34 @@
+class Hero {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque = "";
+
+    switch (this.tipo) {
+      case "mago":
+        ataque = "magia";
+        break;
+      case "guerreiro":
+        ataque = "espada";
+        break;
+      case "monge":
+        ataque = "artes marciais";
+        break;
+      case "ninja":
+        ataque = "shuriken";
+        break;
+      default:
+        ataque = "um ataque desconhecido";
+    }
+
+    console.log(`o ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+// Exemplo de uso da classe Hero
+const meuHeroi = new Hero("Herói", 25, "mago");
+meuHeroi.atacar();
